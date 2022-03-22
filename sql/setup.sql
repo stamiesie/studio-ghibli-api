294531703d076cad41b6c91865ed6ccc20401e87
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-03-22 13:11:27 -0700

    add JSON type to voice_actor in characters table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS characters CASCADE;
+
+CREATE TABLE characters (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    film TEXT NOT NULL,
+    year_of_film INTEGER,
+    voice_actor JSON NOT NULL,
+    about TEXT NOT NULL,
+    photo_url TEXT NOT NULL
+)
