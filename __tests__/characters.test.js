23a22fc0a9b21a74f9096d122c599ee612a87777
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-03-23 12:43:32 -0700

    simplify getCharactersById test

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -1,5 +1,4 @@
 const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const { execSync } = require('child_process');
